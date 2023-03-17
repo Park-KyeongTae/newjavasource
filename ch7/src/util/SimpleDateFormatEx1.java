ae6325a31b7772d0ff4676668e69742de5525a79
kyungtae <parkkt96@naver.com>
2023-03-17 09:48:19 +0900

    주석 추가

--- ch7/src/util/SimpleDateFormatEx1.java
+++ ch7/src/util/SimpleDateFormatEx1.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class SimpleDateFormatEx1 {
 
 	public static void main(String[] args) {
-		
+		//날짜와 시간와 시간 객체
 		Date date = new Date();
 		System.out.println(date);
 		//날짜와 시간을 특정 형식에 만춰 출력 가능하게 하는 클래스
